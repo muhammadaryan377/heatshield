f4451138590d5b5fec948d32f984b1075e27dd89
Muhammad Aryan <arianmalik377@gmail.com>
2026-08-22 14:05:27 +0500

    style: load sidebar visibility controls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './functional.css'
 import './site-builder-v2.css'
+import './sidebar-collapse.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
