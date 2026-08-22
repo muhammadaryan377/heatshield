b11489dde7e3653be33de2973a8f9aea9252db10
Muhammad Aryan <arianmalik377@gmail.com>
2026-08-22 21:24:33 +0500

    style: load product polish layer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import './add-worker-v2.css'
 import './plan.css'
 import './sites-reports.css'
 import './thermal-map.css'
+import './product-polish.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
