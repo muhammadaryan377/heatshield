0403ec6f3160996273c0c26f636bcd5816444991
Muhammad Aryan <arianmalik377@gmail.com>
2026-08-22 18:36:07 +0500

    style: load thermal explorer interface

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import './sidebar-collapse.css'
 import './add-worker-v2.css'
 import './plan.css'
 import './sites-reports.css'
+import './thermal-map.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
