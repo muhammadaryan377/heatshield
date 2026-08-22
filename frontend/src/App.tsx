e4d7035475c13adf3cbd7eb0ccb45cbc1d9b775e
Muhammad Aryan <arianmalik377@gmail.com>
2026-08-22 13:05:13 +0500

    feat: route Add Worker to functional worker creation screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AddWorkerPage } from './pages/AddWorkerPage'
 import { BuildQueuePage } from './pages/BuildQueuePage'
 import { SiteIntelligencePage } from './pages/SiteIntelligencePage'
 
@@ -7,9 +8,9 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SiteIntelligencePage />} />
-        <Route path="/workers/new" element={<BuildQueuePage />} />
+        <Route path="/workers/new" element={<AddWorkerPage />} />
         <Route path="/plan" element={<BuildQueuePage />} />
-        <Route path="/sites" element={<BuildQueuePage />} />
+        <Route path="/sites" element={<SiteIntelligencePage />} />
         <Route path="/reports" element={<BuildQueuePage />} />
         <Route path="/settings" element={<BuildQueuePage />} />
       </Routes>
