8f8a2deb5d0c7c98ed0477184ad3f44d29f00610
Muhammad Aryan <arianmalik377@gmail.com>
2026-08-22 18:28:16 +0500

    feat: route Sites and Reports to production screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AddWorkerPage } from './pages/AddWorkerPage'
 import { BuildQueuePage } from './pages/BuildQueuePage'
 import { GeneratePlanPage } from './pages/GeneratePlanPage'
+import { ReportsPage } from './pages/ReportsPage'
 import { SiteIntelligencePage } from './pages/SiteIntelligencePage'
+import { SitesPage } from './pages/SitesPage'
 
 export default function App() {
   return (
@@ -11,8 +13,8 @@ export default function App() {
         <Route path="/" element={<SiteIntelligencePage />} />
         <Route path="/workers/new" element={<AddWorkerPage />} />
         <Route path="/plan" element={<GeneratePlanPage />} />
-        <Route path="/sites" element={<SiteIntelligencePage />} />
-        <Route path="/reports" element={<BuildQueuePage />} />
+        <Route path="/sites" element={<SitesPage />} />
+        <Route path="/reports" element={<ReportsPage />} />
         <Route path="/settings" element={<BuildQueuePage />} />
       </Routes>
     </BrowserRouter>
