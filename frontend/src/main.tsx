15b00ba7f88c0dc905c36dd7526e3339fed3afe6
Muhammad Aryan <arianmalik377@gmail.com>
2026-08-24 11:15:00 +0500

    style: load integrated workforce intelligence styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,13 +6,16 @@ import './functional.css'
 import './site-builder-v2.css'
 import './sidebar-collapse.css'
 import './add-worker-v2.css'
+import './add-worker-intelligence.css'
 import './plan.css'
 import './sites-reports.css'
 import './thermal-map.css'
 import './product-polish.css'
 import './fortyguard-intelligence.css'
 import './operational-planner.css'
+import './operational-planner-v2.css'
 import './historical-heat.css'
+import './historical-heat-intelligence.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
